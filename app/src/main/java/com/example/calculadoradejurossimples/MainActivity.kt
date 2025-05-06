8b39c93c73a6ab28c4ff4ef3e970d11ca9274a2c
aNgElOfThEnIgH <jotapewp2021@gmail.com>
2025-05-06 14:03:36 -0300

    Commit inicial do JP

---
+++ app/src/main/java/com/example/calculadoradejurossimples/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.calculadoradejurossimples
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+            val rbtCapital = findViewById<RadioButton>(R.id.rbtCapital)
+            val rbtTaxa = findViewById<RadioButton>(R.id.rbtTaxa)
+            val rbtPrazo = findViewById<RadioButton>(R.id.rbtPrazo)
+            val rbtJuros = findViewById<RadioButton>(R.id.rbtJuros)
+            val txtCapital = findViewById<EditText>(R.id.txtCapital)
+            val txtTaxa = findViewById<EditText>(R.id.txtTaxa)
+            val txtPrazo = findViewById<EditText>(R.id.txtPrazo)
+            val txtJuros = findViewById<EditText>(R.id.txtJuros)
+            val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+            val btnLimpar = findViewById<Button>(R.id.btnLimpar)
+
+            // Marcar "Juros" como padrão
+            rbtJuros.isChecked = true
+
+            // Função para atualizar campos
+            fun atualizarCampos() {
+                txtCapital.isEnabled = rbtCapital.isChecked.not()
+                txtTaxa.isEnabled = rbtTaxa.isChecked.not()
+                txtPrazo.isEnabled = rbtPrazo.isChecked.not()
+                txtJuros.isEnabled = rbtJuros.isChecked.not()
+            }
+
+            // Atualizar campos inicialmente
+            atualizarCampos()
+
+            // Quando qualquer RadioButton for clicado
+            rbtJuros.setOnClickListener { atualizarCampos() }
+            rbtCapital.setOnClickListener { atualizarCampos() }
+            rbtTaxa.setOnClickListener { atualizarCampos() }
+            rbtPrazo.setOnClickListener { atualizarCampos() }
+            //Evellyn daqui pra baixo
+
+            //Vitinho daqui pra baixo
+
+                //Kauan daqui pra baixo
+
+            }
+        }
+    }
